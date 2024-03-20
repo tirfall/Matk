a011ca27f54b43c9f72953365945251ae91d670d
opilane <opilane@HP-CZC2349HTB>
2024-03-20 15:03:16 +0200

    1.4

--- Matk/Vilsandi.xaml.cs
+++ Matk/Vilsandi.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace Matk
 	{
 		public Vilsandi ()
 		{
-            InitializeComponent();
+			Title = "Vilsandi";
             Grid grd = new Grid
 			{
 				RowDefinitions =
@@ -29,12 +30,24 @@ namespace Matk
 				}
             };
 			Label nimetus = new Label { Text = "Vilsandi matkarada", FontSize = 30 };
-			Image img = new Image { Source = "jaanuar.jpg" };
-			Label kirjeldus = new Label { Text = "Matkarada" };
+			ImageButton img = new ImageButton {
+                BackgroundColor = Color.Transparent,
+                HeightRequest = 200,
+                WidthRequest = 200,
+                Source = ImageSource.FromStream(() => new MemoryStream(Properties.Resources.vilsandi))
+			};
+			img.Clicked += imgClicked;
+			Label kirjeldus = new Label { Text = "Vilsandi saare keskelt vaatetorni alt Bioloogiajaama lähedusest saavad alguse 2 matkarada, pikkustega 6 km ja 8 km." };
 			grd.Children.Add(nimetus,0,0);
 			grd.Children.Add(img,0,1);
 			grd.Children.Add(kirjeldus,0,2);
 			Content= grd;
 		}
-	}
+
+        private void imgClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.loodusegakoos.ee/kuhuminna/rahvuspargid/vilsandi-rahvuspark/9264"));
+        }
+    }
+
 }
